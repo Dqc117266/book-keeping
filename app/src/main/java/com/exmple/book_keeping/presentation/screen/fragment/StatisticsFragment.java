eef06719496b83fc27c2b14ef38d1dd286a79248
duqichun <dqc1172662142@gmail.com>
2024-06-17 18:01:06 +0800

    提交fragment

--- app/src/main/java/com/exmple/book_keeping/presentation/screen/fragment/StatisticsFragment.java
+++ app/src/main/java/com/exmple/book_keeping/presentation/screen/fragment/StatisticsFragment.java
@@ -5,11 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.exmple.book_keeping.databinding.FragmentBillBinding;
 import com.exmple.book_keeping.databinding.FragmentStatisticsBinding;
 
 public class StatisticsFragment extends Fragment {
